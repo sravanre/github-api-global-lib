bc26ecd2cff5fc1fdb0af57a9555358c86df03f9
Your Name <you@example.com>
2022-10-26 13:54:17 +0530

    adding parameters

--- vars/helloWordSravan.groovy
+++ vars/helloWordSravan.groovy
@@ -1,7 +1,10 @@
-def call() {
-    sh '''
-    echo hello world
-    echo Sravan is a good boy
-    hostname
-    '''
-}
+def call(String name,
+        String dayOfWeek) 
+        {
+            sh '''
+            echo hello world
+            echo Sravan is a good boy
+            hostname
+            echo Hello ${name}. Today is ${dayOfWeek}
+            '''
+        }
